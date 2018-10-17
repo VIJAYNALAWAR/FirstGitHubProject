8835351a1c6692db7723f071cad99f9f4b693ba4
VNALAWAR <VNALAWAR@192.168.1.9>
2018-10-17 22:53:16 +0530

    Printed "GitHub Project is Created"

---
+++ New Selenium/src/Sample_Data/New_GitHub.java
@@ -0,0 +1,12 @@
+package Sample_Data;
+
+public class New_GitHub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("GitHub Project is Created");
+
+	}
+
+}
